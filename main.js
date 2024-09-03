c7d93de120f8f7fd3a831c944be6f561d75e8b35
Iryna <VacX_vb15@student.itstep.org>
2024-09-04 01:18:15 +0300

    numbers and lines

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+let firstStr = prompt("First string");
+let secondStr = prompt("Second string");
+let thirdStr = prompt("Third string");
 
-
+console.log(`${firstStr} // ${secondStr} // ${thirdStr}`);
