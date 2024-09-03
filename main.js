826c3f2b3588e8662021e4b9a120c605582f18a4
Iryna <VacX_vb15@student.itstep.org>
2024-09-04 01:07:39 +0300

    list of types

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+let numberType = 13;
+let stringType = "KittyCat";
+let booleanType = false;
+let nullType = null;
+let functionType = function() { };
+let undefinedType;
 
-
+console.log("numberType:", typeof numberType);
+console.log("stringType:", typeof stringType);
+console.log("booleanType:", typeof booleanType);
+console.log("nullType:", typeof nullType);
+console.log("functionType:", typeof functionType);
+console.log("undefinedType:", typeof undefinedType);
