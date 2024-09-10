64f710a61000d520914324ca21bdeb2f03a2a1f8
Iryna <VacX_vb15@student.itstep.org>
2024-09-10 20:56:22 +0300

    simple number

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+let value = +prompt("Enter an integer greater than 1 :");
 
-
+if(!isNaN(value) && Number.isInteger(value) && value > 1) {
+    let isSimple = true;
+    for(let i = 2; i < value; i++) {
+        if(value % i === 0) {
+            isSimple = false;
+            break;
+        }
+    }
+    if(isSimple) {
+        console.log("This is a simple number");
+    }
+    else {
+        console.log("This isn't a simple number");
+    }
+}
+else {
+    console.log("Invalid value");
+}
