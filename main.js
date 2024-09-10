23c40dea1d86bfb974375e78d8e94686c5d36358
Iryna <VacX_vb15@student.itstep.org>
2024-09-10 20:44:07 +0300

    finding the right numbers

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+let value = +prompt("Enter an integer :");
 
-
+if(!isNaN(value) && Number.isInteger(value)) {
+    for(let i = 1; i <= 100; i++) {
+        if(i * i <= value) {
+            console.log(i);
+        }
+    }
+}
+else {
+    console.log("Invalid value");
+}
