b714feb117bd4a90791b61b3814aa7eeb00d090e
Iryna <VacX_vb15@student.itstep.org>
2024-09-13 20:42:50 +0300

    calculate arithmetic mean value

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+function calculateArithmeticMeanValue(arr) {
+    let onlyNumArr = arr.filter(element => typeof element === "number");
 
+    if(onlyNumArr.length === 0) {
+        return "No numbers";
+    }
 
+    return onlyNumArr.reduce((acc, num) => acc + num) / onlyNumArr.length;
+}
+
+let arr = ["qwe", 13, true, 43, 'u', [1,2,3], 103];
+console.log(calculateArithmeticMeanValue(arr));
