53c8871bdbca6c55f7ce99e9c40ae88ee4b4c217
Iryna <VacX_vb15@student.itstep.org>
2024-09-13 20:26:13 +0300

    remove symbols function

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+function removeSymbols(str, arrSymbols) {
+    return str ? str.split('').filter(element => !arrSymbols.includes(element)).join('') : "Empty string";
+}
 
+let str = prompt("Enter a string :");
+let arrSymbols = [...new Set(prompt("Enter symbols to remove :"))];
 
+console.log(removeSymbols(str, arrSymbols));
