c6f88109a8e12c9e64328e63f8ed73329a700692
Iryna <VacX_vb15@student.itstep.org>
2024-09-17 21:01:58 +0300

    curry

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+function calcProductTwoNumbers(x) {
+    return (y) => {
+      return x * y;
+    };
+}
 
-
+console.log(calcProductTwoNumbers(5)(3)); 
+console.log(calcProductTwoNumbers(3)(4)); 
+console.log(calcProductTwoNumbers(4)(5)); 
