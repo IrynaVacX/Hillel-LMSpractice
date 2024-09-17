74dc10a0b9166f520ef1897b1a5a3eee9ea71556
Iryna <VacX_vb15@student.itstep.org>
2024-09-17 21:35:33 +0300

    loop & function

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+function inputNumber() {
+    let value;
+    for(let i = 0; i < 10; i++) {
+        value = prompt(`Enter a number greater than 100 (iteration - ${i+1}):`);
+        if(isNaN(+value) || +value > 100) {
+            break;
+        }
+    }
+    console.log(value);
+};
 
-
+inputNumber();
