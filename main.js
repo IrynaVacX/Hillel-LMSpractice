f593030cb306d160e925d44cb8522349302faca6
Iryna <VacX_vb15@student.itstep.org>
2024-09-17 20:55:41 +0300

    locking

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
+function calcSum() {
+    let sum = 0;
+    return function (value) {
+        sum += value;
+        return sum;
+    }
+};
 
+const sum = calcSum();
 
+console.log(sum(4)); 
+console.log(sum(6)); 
+console.log(sum(10)); 
+console.log(sum(7)); 
