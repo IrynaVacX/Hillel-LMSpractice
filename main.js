ccb91f8ee23716fc9a09bd58b6c3d8cab39290e1
Iryna <VacX_vb15@student.itstep.org>
2024-09-24 20:38:00 +0300

    ladder

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+let ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        console.log(this.step);
+        return this;
+    },
+};
 
-
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.up().up().down().showStep()  // 2
+    .down().up().up().showStep();   // 3
