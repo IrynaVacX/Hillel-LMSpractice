99af1adb9e6fa68f212679fd8b7a035664f7650a
Iryna <VacX_vb15@student.itstep.org>
2024-09-27 20:33:17 +0300

    total salary

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+let company = {
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    development: {
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+        internals: [{name: 'Jack', salary: 1300}],
+    }
+}
 
+function TotalSalary(obj) {
+    let sum = 0;
+    if(Array.isArray(obj)) {
+        obj.forEach(element => {
+            sum += element.salary;
+        });
+    }
+    else if(typeof obj === 'object') {
+        for(let department in obj) {
+            sum += TotalSalary(obj[department]);
+        }
+    }
+    return sum;
+}
 
+console.log(TotalSalary(company));
