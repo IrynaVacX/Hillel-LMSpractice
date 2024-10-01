a560aba7f00eaa65dc4cf6d60e4f4f9f95e2ec42
Iryna <VacX_vb15@student.itstep.org>
2024-10-01 21:41:42 +0300

    contacts book

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+let contactsBook = {
+    contactsList: [
+        {name: "Name1", phone: "0672346712", email: "name111@gmail.com"},
+        {name: "Name2", phone: "0683467234", email: "name222@gmail.com"},
+        {name: "Name3", phone: "0967128093", email: "name333@gmail.com"},
+    ],
+    searchContact: function(name) {
+        return this.contactsList.find(element => element.name.toLowerCase() === name.toLowerCase());
+    },
+    addContact: function(name, phone, email) {
+        this.contactsList.push({name: name, phone: phone, email: email});
+    },
+}
 
-
+console.log(contactsBook.searchContact("Name2"));
+contactsBook.addContact("Name4", "0673451200", "name444@gmail.com");
+console.log(contactsBook.searchContact("name4"));
