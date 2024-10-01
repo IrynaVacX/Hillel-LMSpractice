8ee4325141f9ead1d70c60c7773f9ba404cac503
Iryna <VacX_vb15@student.itstep.org>
2024-10-01 21:34:34 +0300

    user info

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+let user = {
+    name: "KittyCat",
+    age: 21,
+    location: "Laplandiya",
+    displayInfo: function() {
+        console.log(`name - ${this.name} / age - ${this.age} / location - ${this.location}`);
+    }
+};
 
-
+user.displayInfo();
