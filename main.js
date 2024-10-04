76d4936606b2b30e94b8984ab48f9aff1d71b14e
Iryna <VacX_vb15@student.itstep.org>
2024-10-04 21:25:13 +0300

    display random image

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    getRandomImage();
+});
 
-
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * 9) + 1;
+    const image = document.createElement('img');
+    image.src = './memes/' + randomIndex + ".jpg";
+    image.style.width = '484px';
+    document.body.appendChild(image);
+}
