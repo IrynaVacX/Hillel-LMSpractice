39d72b1db5fde1173e785ba1f3b8cadce08ce3d6
Iryna <VacX_vb15@student.itstep.org>
2024-10-04 21:21:40 +0300

    Pythagorean table

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    displayPifagorTable();
+});
 
-
+function displayPifagorTable() {
+    const table = document.createElement("table");
+    const tBody = document.createElement("tBody");
+    for(let i = 0; i <= 10; i++) {
+        const newRow = document.createElement("tr");
+        for(let j = 0; j <= 10; j++) {
+            const newColumn = document.createElement("td");
+            if(i === 0 || j === 0) {
+                newColumn.textContent = i ? i : j;
+                newColumn.setAttribute("align", "center");
+                newColumn.setAttribute("bgcolor", "lightsalmon");
+                newRow.appendChild(newColumn);
+                continue;
+            }
+            newColumn.textContent = i * j;
+            newColumn.setAttribute("align", "center");
+            newColumn.setAttribute("bgcolor", (j + i) % 2 ? "bisque" : "burlywood");
+            newRow.appendChild(newColumn);
+        }
+        tBody.appendChild(newRow);
+    }
+    table.appendChild(tBody);
+    table.setAttribute("border", "2");
+    table.setAttribute("cellpadding", "5");
+    document.body.appendChild(table);
+}
