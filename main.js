26c86b2aef55a6f6a22dc99a9b3194616e4a34ff
Iryna <VacX_vb15@student.itstep.org>
2024-10-09 13:38:41 +0300

    events

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonsContainer = document.getElementById("container");
+  buttonsContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === "BUTTON") {
+      alert(e.target.name);
+    }
+  });
+});
