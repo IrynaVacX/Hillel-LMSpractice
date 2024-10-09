24a011c4b357f4872c4cb834ede3609affd71889
Iryna <VacX_vb15@student.itstep.org>
2024-10-09 13:34:03 +0300

    redirect link

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+promptButton.addEventListener("click", () => {
+  const link = prompt("Enter a link : ");
+  if (link) {
+    localStorage.setItem("redirectLink", link);
+  }
+});
 
-
+redirectionButton.addEventListener("click", () => {
+  const savedLink = localStorage.getItem("redirectLink");
+  if (savedLink) {
+    window.location.href = savedLink;
+  }
+  else {
+    alert("no link");
+  }
+});
