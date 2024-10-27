84a620c403c90da54caaac7d83af8964adfd2efe
Iryna <VacX_vb15@student.itstep.org>
2024-10-27 03:36:46 +0300

    calculator

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-
-
+class Calculator {
+    add(n1, n2) {
+        return n1 + n2;
+    }
+    subtract(n1, n2) {
+        return n1 - n2;
+    }
+    multiply(n1, n2) {
+        return n1 * n2;
+    }
+    divide(n1, n2) {
+        if(n2 === 0) {
+            return "Cannot divide by zero";
+        }
+        return n1 / n2;
+    }
+}
+   
+const calc = new Calculator();
+   
+console.log(calc.add(5, 3)); // 8
+console.log(calc.subtract(10, 4)); // 6
+console.log(calc.multiply(3, 6)); // 18
+console.log(calc.divide(8, 2)); // 4
