0966490d40acaa19682dd42938ddcedb9412ef74
Iryna <VacX_vb15@student.itstep.org>
2024-10-31 03:19:34 +0200

    timer

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+"use strict";
+const timeValue = 133;
 
+function startTimer(timeLeft) {
+  const timerElement = document.getElementById("timer");
 
+  const interval = setInterval(() => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+
+    timerElement.textContent = (minutes < 10 ? "0" : "") + 
+      minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+    timeLeft--;
+
+    if (timeLeft < 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+startTimer(timeValue);
